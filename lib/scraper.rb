8c4628b00b9b380e45b821a02a7ffec5fe1b25f2
Lauren Abrams <lauren.abrams@powerhrg.com>
2018-06-03 16:51:24 -0400

    finished student scraper lab

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,12 +4,39 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    index_page = Nokogiri::HTML(open(index_url))
+    students = []
+
+    index_page.css("div.roster-cards-container").each do |roster_card|
+      roster_card.css(".student-card a").each do |student|
+        student_name = student.css('.student-name').text
+        location = student.css('.student-location').text
+        profile_link = "#{student.attr('href')}"
+        students << {name: student_name, location: location, profile_url: profile_link}
+      end
+    end
+    students
   end
 
   def self.scrape_profile_page(profile_url)
-    
-  end
+    profile_page = Nokogiri::HTML(open(profile_url))
+    student= {}
+    links = profile_page.css(".social-icon-container").children.css("a").collect { |social_page| social_page.attribute('href').value}
+    links.each do |link|
+      if link.include?("linkedin")
+        student[:linkedin] = link
+      elsif link.include?("twitter")
+        student[:twitter] = link
+      elsif link.include?("github")
+        student[:github] = link
+      else student[:blog] = link
+      end
+    end
+    student[:profile_quote] = profile_page.css(".profile-quote").text if profile_page.css(".profile-quote")
+    student[:bio] = profile_page.css("div.bio-content.content-holder div.description-holder p").text if profile_page.css("div.bio-content.content-holder div.description-holder p")
 
+    student
+  end
 end
+
 
